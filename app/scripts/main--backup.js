0786be667f44445939885b54ebec172b548806e2
Joe <josephdburdick@gmail.com>
2015-10-21 18:04:05 -0400

    Add bottom of page speedbump animation.

--- app/scripts/main--backup.js
+++ app/scripts/main--backup.js
@@ -6,7 +6,6 @@ $(function(){
         $document = $(document),
         $body = $('body'),
         defaults = {
-          stage: '[data-role="stage"]',
           pageContainerClass: '.page-container',
           mainAreaID: '#page-current',
           prevAreaID:  '#page-prev',
@@ -14,41 +13,33 @@ $(function(){
         },
         settings,
         newPage,
-        _templateSpeedBump = function _templateSpeedBump(options){
-          return ['<div class="page-container--loader-container scene_element '+ options.direction +'" id="'+ options.id +'">',
-                    '<div class="page-container--loader scene_element">LOADING...</div>',
-                  '</div>'].join('\n');
-        },
-        _loadSpeedBump = function _loadSpeedBump(direction){
-          var $speedBumpEl = null,
-              id = settings.nextAreaID.replace('#', ''),
-              speedBumpHTML = _templateSpeedBump({ id: id, direction: direction }),
-              speedBump = $.parseHTML(speedBumpHTML);
-
-          if (!$('#' + $(speedBump).attr('id')).length){
-            if (direction === "append") {
-              $(settings.stage).append(speedBumpHTML);
-            } else {
-              $(settings.stage).prepend(speedBumpHTML);
-            }
-            $speedBumpEl = $('#' + $(speedBump).attr('id'));
-
-            $speedBumpEl.addClass('scene_element--fadeinup');
-            // setTimeout(function(){
-            //   $speedBumpEl.find('.page-container--loader').addClass('scene_element--fadeinup');
-            // }, 1000);
-          } else {
-            return false;
-          }
-        },
+        // <div class="page-container--previous fixed-position" id="page-prev">
+        //   <div class="container">
+        //     <section>
+        //       <header class="page-title">
+        //         <h3 class="text-center">Previous</h3>
+        //       </header>
+        //     </section>
+        //   </div>
+        // </div>
+        // <div class="page-container--next fixed-position" id="page-next" data-url="about.html">
+        //   <div class="container">
+        //     <section>
+        //       <header class="page-title">
+        //         <h3 class="text-center">Next</h3>
+        //       </header>
+        //     </section>
+        //   </div>
+        // </div>
         // routes = ['index', 'work', 'services', 'about', 'thoughts'],
         getHeightandOffset = function getHeightandOffset(el){
           return $(el).outerHeight(true) + $(el).offset().top;
         },
         distanceFromPageTop = function distanceFromPageTop(el){
-          return $(el).offset().top;
+          return getHeightandOffset(el);
         },
         triggerPageTransition = function triggerPageTransition(transition){
+
           if (transition.direction === "prepend"){
             setTimeout(function(){
               $('.scene_element').not('#page-current').addClass('scene_element--fadeindown');
@@ -59,36 +50,36 @@ $(function(){
               $('.scene_element').not('#page-current').addClass('scene_element--fadeinup');
             }, 1000);
           }
+
         },
-        _isHidden = function _isHidden(el){
-          return $(el).css('opacity') === 0;
-        },
-        _onScroll = function _onScroll(){
-
-          $window.on('scroll', function(ev){
-            var thisScrollTop = Math.round($(this).scrollTop()),
-                thisInnerHeight = Math.round($(this).innerHeight());
-
-            if(thisScrollTop === 0) {
-              console.log("Reached beginning of page.");
-              // _loadNewContent('prepend');
-            }
-
-            if($window.scrollTop() + $window.height() > $document.height() - 60) {
-              console.log('Near end of page.');
-            }
-
-            if($window.scrollTop() + $window.height() === $document.height()) {
-              console.log('Reached end of page.');
-              setTimeout(function(){
-                _loadSpeedBump('append');
-              },1000);
-
-              // _loadNewContent('append');
-            }
-
-          });
-        },
+        // _isHidden = function _isHidden(el){
+        //   return $(el).css('opacity') === 0;
+        // },
+        // _onScroll = function _onScroll(){
+        //
+        //   $window.on('scroll', function(ev){
+        //     console.log(ev, settings);
+        //
+        //     var thisScrollTop = Math.round($(this).scrollTop()),
+        //         thisInnerHeight = Math.round($(this).innerHeight());
+        //
+        //     if(thisScrollTop === 0) {
+        //       console.log("Reached beginning of page.");
+        //       _loadNewContent('prepend');
+        //     }
+        //
+        //     if($window.scrollTop() + $window.height() > $document.height() - 60) {
+        //       console.log('Near end of page.');
+        //     }
+        //
+        //     if($window.scrollTop() + $window.height() === $document.height()) {
+        //       console.log('Reached end of page.');
+        //
+        //       _loadNewContent('append');
+        //     }
+        //
+        //   });
+        // },
         _loadNewContent = function _loadNewContent(direction){
           var clonePage;
 
@@ -114,6 +105,7 @@ $(function(){
         _render = function _render(){
           // var bodyOffsetTop = distanceFromPageTop(settings.prevAreaID);
           // $window.scrollTop(bodyOffsetTop);
+
           setTimeout(function(){
             $window.on('scroll', _onScroll());
           }, 500);
@@ -122,20 +114,16 @@ $(function(){
             marginTop: $(settings.prevAreaID).outerHeight(),
             marginBottom: $(settings.nextAreaID).outerHeight()
           }).addClass('active');
+
         },
         init = function init(options){
           settings = $.extend({}, defaults, options);
-          _render();
-          $window.load(function(){
-            $window.scrollTop($('[data-page] .page-block').offset().top);
-          });
+          $window.on('load', _render());
         };
 
     return {
       init: init,
-      triggerPageTransition: triggerPageTransition,
-      getHeightandOffset: getHeightandOffset,
-      distanceFromPageTop: distanceFromPageTop
+      triggerPageTransition: triggerPageTransition
     };
   })();
 
